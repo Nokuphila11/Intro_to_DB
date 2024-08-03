b5d73f70037ed63a565e6c9968946481f02ed6b7
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-08-03 17:47:47 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -40,3 +40,13 @@ CREATE TABLE Orders (
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KE
 
+    CREATE TABLE Order_Details (
+  orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE,
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+
